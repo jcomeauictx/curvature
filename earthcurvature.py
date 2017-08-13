4748a731d2ec4d03cc5c08c75f1d34fa8ed431a8
John Comeau <jc@unternet.net>
2017-08-13 10:12:45 -0700

    accounts for refraction

--- earthcurvature.py
+++ earthcurvature.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python -OO
 '''
 calculate curvature by various methods
+
+none of the "curvature" websites referenced below take into account
+atmospheric refraction. you can include it by setting the environment
+variable COEFFICIENT_OF_REFRACTION, typically to about .25:
+
+    jcomeau@aspire:~$ earthcurvature.py 10
+    drop in 10.00000000 miles is 66.68347048 feet
+    jcomeau@aspire:~$ COEFFICIENT_OF_REFRACTION=.25 earthcurvature.py 10
+    drop in 10.00000000 miles is 50.01261449 feet
+
+often you will see the coefficient expressed as 4/3. this is because they
+have already inverted it using 1/(1-k).
 '''
 from __future__ import print_function, division
 import sys, os, math, logging
 logging.basicConfig(level=logging.DEBUG if __debug__ else logging.INFO)
 R = RADIUS = 3959  # miles, per earthcurvature.com
+K = float(os.getenv('COEFFICIENT_OF_REFRACTION') or '0.0')
+ER = EFFECTIVE_RADIUS = RADIUS / (1 - K)
 KM = 1.60934  # conversion factor, miles to kilometers, from Google
 UNITS = ['mm', 'cm', 'm', 'km', 'inches', 'feet', 'yards', 'miles']
 
@@ -19,10 +33,10 @@ def earthcurvature(distance=1, unit='miles', dropunit='feet', height=0):
     by this formula, but will be by the dizzib method.
     '''
     distance = float(distance)
-    c = 2 * math.pi * RADIUS
+    c = 2 * math.pi * ER
     d = miles(distance, unit)
     a = (360 / c) * d
-    h = convert(RADIUS * (1 - math.cos(math.radians(a))), dropunit)
+    h = convert(ER * (1 - math.cos(math.radians(a))), dropunit)
     logging.debug('locals(): %s', locals())
     return 'drop in %.08f %s is %.08f %s', (distance, unit, h, dropunit)
 
@@ -50,9 +64,9 @@ def dizzib(distance=1, unit='miles', dropunit='feet', height=0):
     distance, height = float(distance), float(height)
     d0 = miles(distance, unit)
     h0 = miles(height, dropunit)
-    d1 = math.sqrt((h0 ** 2) + (2 * R * h0))
+    d1 = math.sqrt((h0 ** 2) + (2 * ER * h0))
     d2 = d0 - d1
-    h1 = convert(math.sqrt((d2 ** 2) + (R ** 2)) - R, dropunit)
+    h1 = convert(math.sqrt((d2 ** 2) + (ER ** 2)) - ER, dropunit)
     logging.debug('locals(): %s', locals())
     format_string = 'distance to horizon: %.08f %s, hidden height: %.08f %s'
     return format_string, (d1, unit, h1, dropunit)
@@ -152,5 +166,7 @@ if __name__ == '__main__':
     # all curvature functions should have same args as earthcurvature
     DEFAULTS = FUNCTION.func_defaults
     ARGS[len(ARGS):] = DEFAULTS[len(ARGS):]
+    logging.debug('effective radius: %.1f, radius: %.1f, ratio: %.3f',
+                  ER, R, (ER / R))
     FORMAT_STRING, RESULTS = FUNCTION(*ARGS)
     print(FORMAT_STRING % RESULTS)
